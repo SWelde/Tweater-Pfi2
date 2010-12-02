ceddc565616353f3b094dde7d47c4ac8711070c8
agoransson <a.goransson@1scale1.com>
2010-12-02 20:33:34 +0100

    Replaced the MigLayout with GridBag.
    Signed-off-by: agoransson <a.goransson@1scale1.com>

--- MyTweetListener/src/com/andreas/pfi2examples/twitterstream/LoginPanel.java
+++ MyTweetListener/src/com/andreas/pfi2examples/twitterstream/LoginPanel.java
@@ -8,7 +8,9 @@ import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
-import net.miginfocom.swing.MigLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 /**
  * Just a simple Login widget. To make the Main.java implementation
@@ -30,23 +32,54 @@ public class LoginPanel extends JPanel {
 		setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null,
 				null), "Login", TitledBorder.LEADING, TitledBorder.TOP, null,
 				null));
-		setLayout(new MigLayout("", "[][][grow]", "[][][]"));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 120, 123, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 1.0,
+				Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
 
 		JLabel lblLogin = new JLabel("Username");
-		add(lblLogin, "cell 1 1,alignx trailing");
+		GridBagConstraints gbc_lblLogin = new GridBagConstraints();
+		gbc_lblLogin.anchor = GridBagConstraints.WEST;
+		gbc_lblLogin.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLogin.gridx = 0;
+		gbc_lblLogin.gridy = 0;
+		add(lblLogin, gbc_lblLogin);
 
 		usernameField = new JTextField();
-		add(usernameField, "cell 2 1,growx");
+		GridBagConstraints gbc_usernameField = new GridBagConstraints();
+		gbc_usernameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_usernameField.insets = new Insets(0, 0, 5, 5);
+		gbc_usernameField.gridx = 1;
+		gbc_usernameField.gridy = 0;
+		add(usernameField, gbc_usernameField);
 		usernameField.setColumns(10);
 
 		JLabel lblPassword = new JLabel("Password");
-		add(lblPassword, "cell 1 2,alignx trailing");
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.anchor = GridBagConstraints.WEST;
+		gbc_lblPassword.insets = new Insets(0, 0, 0, 5);
+		gbc_lblPassword.gridx = 0;
+		gbc_lblPassword.gridy = 1;
+		add(lblPassword, gbc_lblPassword);
 
 		passwordField = new JPasswordField();
-		add(passwordField, "flowx,cell 2 2,growx");
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.insets = new Insets(0, 0, 0, 5);
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 1;
+		add(passwordField, gbc_passwordField);
 
 		btnReadTweetSteam = new JButton("Read Tweet Steam");
-		add(btnReadTweetSteam, "cell 2 2");
+		GridBagConstraints gbc_btnReadTweetSteam = new GridBagConstraints();
+		gbc_btnReadTweetSteam.fill = GridBagConstraints.BOTH;
+		gbc_btnReadTweetSteam.gridheight = 2;
+		gbc_btnReadTweetSteam.gridx = 2;
+		gbc_btnReadTweetSteam.gridy = 0;
+		add(btnReadTweetSteam, gbc_btnReadTweetSteam);
 
 	}
 
