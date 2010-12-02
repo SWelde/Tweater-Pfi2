58c9d68ff5f4c4f0fb09b52c6ddd9a824e6aff9b
agoransson <a.goransson@1scale1.com>
2010-12-03 00:05:03 +0100

    Exercise one and two, the login and about problem.
    Signed-off-by: agoransson <a.goransson@1scale1.com>

---
+++ The Login Problem/src/mah/k3/pfi2/loginproblem/MainWindow.java
@@ -0,0 +1,299 @@
+package mah.k3.pfi2.loginproblem;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.GridBagLayout;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
+
+public class MainWindow extends JFrame {
+
+	/* Login management */
+	private Users users = new Users();
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 328);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+
+		JMenu mnFile_1 = new JMenu("File");
+		mnFile.add(mnFile_1);
+
+		JMenuItem mntmNewUser = new JMenuItem("New user");
+		mnFile_1.add(mntmNewUser);
+
+		JMenuItem mntmFile = new JMenuItem("Save user");
+		mnFile_1.add(mntmFile);
+
+		JMenuItem mntmDeleteUser = new JMenuItem("Delete user");
+		mnFile_1.add(mntmDeleteUser);
+
+		JSeparator separator = new JSeparator();
+		mnFile.add(separator);
+
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+		JScrollPane scrollPane = new JScrollPane();
+
+		JButton btnDeleteUser = new JButton("Delete user");
+
+		JButton btnSaveUser = new JButton("Save user");
+
+		JButton btnDeleteUsert = new JButton("New user");
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED,
+				null, null), "User", TitledBorder.LEADING, TitledBorder.TOP,
+				null, null));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addContainerGap()
+										.addComponent(scrollPane,
+												GroupLayout.PREFERRED_SIZE,
+												143, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(
+												ComponentPlacement.RELATED)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																Alignment.TRAILING,
+																gl_contentPane
+																		.createParallelGroup(
+																				Alignment.TRAILING,
+																				false)
+																		.addComponent(
+																				btnDeleteUser,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				btnSaveUser,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				btnDeleteUsert,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE))
+														.addComponent(
+																panel,
+																GroupLayout.DEFAULT_SIZE,
+																255,
+																Short.MAX_VALUE))
+										.addContainerGap()));
+		gl_contentPane
+				.setVerticalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								Alignment.TRAILING,
+								gl_contentPane
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.TRAILING)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				panel,
+																				GroupLayout.DEFAULT_SIZE,
+																				146,
+																				Short.MAX_VALUE)
+																		.addPreferredGap(
+																				ComponentPlacement.UNRELATED)
+																		.addComponent(
+																				btnDeleteUsert)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				btnSaveUser)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				btnDeleteUser))
+														.addComponent(
+																scrollPane,
+																GroupLayout.DEFAULT_SIZE,
+																230,
+																Short.MAX_VALUE))
+										.addContainerGap()));
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		JLabel lblName = new JLabel("Name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.anchor = GridBagConstraints.WEST;
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.gridx = 0;
+		gbc_lblName.gridy = 0;
+		panel.add(lblName, gbc_lblName);
+
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+
+		JLabel lblTitle = new JLabel("Title");
+		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+		gbc_lblTitle.anchor = GridBagConstraints.WEST;
+		gbc_lblTitle.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTitle.gridx = 0;
+		gbc_lblTitle.gridy = 1;
+		panel.add(lblTitle, gbc_lblTitle);
+
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		panel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+
+		JLabel lblPosition = new JLabel("Position");
+		GridBagConstraints gbc_lblPosition = new GridBagConstraints();
+		gbc_lblPosition.anchor = GridBagConstraints.WEST;
+		gbc_lblPosition.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPosition.gridx = 0;
+		gbc_lblPosition.gridy = 2;
+		panel.add(lblPosition, gbc_lblPosition);
+
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 2;
+		panel.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+
+		JLabel lblIncome = new JLabel("Income");
+		GridBagConstraints gbc_lblIncome = new GridBagConstraints();
+		gbc_lblIncome.anchor = GridBagConstraints.WEST;
+		gbc_lblIncome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIncome.gridx = 0;
+		gbc_lblIncome.gridy = 3;
+		panel.add(lblIncome, gbc_lblIncome);
+
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 1;
+		gbc_textField_3.gridy = 3;
+		panel.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+
+		JLabel lblNotes = new JLabel("Notes");
+		GridBagConstraints gbc_lblNotes = new GridBagConstraints();
+		gbc_lblNotes.anchor = GridBagConstraints.WEST;
+		gbc_lblNotes.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNotes.gridx = 0;
+		gbc_lblNotes.gridy = 4;
+		panel.add(lblNotes, gbc_lblNotes);
+
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 1;
+		gbc_textField_4.gridy = 4;
+		panel.add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
+
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		contentPane.setLayout(gl_contentPane);
+
+		startloginpanel();
+	}
+
+	/**
+	 * Starts the loginpanel when we're done creating the JFrame.
+	 */
+	private void startloginpanel() {
+		LoginDialog login = new LoginDialog(this);
+		login.setVisible(true);
+	}
+
+	public void login(String username, String password) {
+		/* If we fail to login just close the program */
+		if (!users.login(username, password)) {
+			dispose();
+			System.exit(0);
+		}
+	}
+
+}
